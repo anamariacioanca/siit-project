85401f9f87e0682872938dd5f47f5ae7e5eab8a4
Anamaria Cioanca <anamaria.cioanca.rus@gmail.com>
2023-06-05 10:13:47 +0300

    Add initial model

--- sampleproject/first/models.py
+++ sampleproject/first/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    time = models.IntegerField()
+    price =  models.FloatField()
+    description = models.TextField()
+    name = models.CharField(max_length=30)
