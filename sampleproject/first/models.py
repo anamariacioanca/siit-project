c4174aae8b406781e591ded09ff5099bb9b00a79
Anamaria Cioanca <anamaria.cioanca.rus@gmail.com>
2023-06-21 13:02:29 +0300

    Add unique

--- sampleproject/first/models.py
+++ sampleproject/first/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     time = models.IntegerField(default=80)
-    price =  models.FloatField()
+    price =  models.FloatField(db_index=True)
     description = models.TextField(null=True, blank=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     created = models.DateTimeField(auto_now=True)
     update = models.DateTimeField(auto_now=True) 
 
