aeabaeaa917a27f51ae4dfb9473706b3c3cfe846
Anamaria Cioanca <anamaria.cioanca.rus@gmail.com>
2023-06-21 12:04:19 +0300

    Add default value

--- sampleproject/first/models.py
+++ sampleproject/first/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    time = models.IntegerField()
+    time = models.IntegerField(default=80)
     price =  models.FloatField()
     description = models.TextField()
     name = models.CharField(max_length=30)
