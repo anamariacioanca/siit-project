1837d39187cedcda4ab55ebbaf4c1e163ca11688
Anamaria Cioanca <anamaria.cioanca.rus@gmail.com>
2023-06-23 10:32:33 +0300

    Or ORM example

--- sampleproject/first/views.py
+++ sampleproject/first/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Course
+from django.db.models import Q
 
 
 # Create your views here.
@@ -14,7 +15,7 @@ def courses(request):
     year = request.GET.get("year")
     all_courses = Course.objects.all().order_by("-year")
     if year is not None:   
-        all_courses = all_courses.filter(year__lte=int(year), name_contains="Course")
+        all_courses = all_courses.filter(Q(year__lte=int(year)) | Q(name__icontains="Course"))
     courses_name = [f"<a href='/course/{Course.id}'>{Course.name} year: {Course.year}</a>" for Course in all_courses]
     print(request.GET)
     return HttpResponse(f"{courses_name}")
